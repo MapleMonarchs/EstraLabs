c408d9301aa81e4b58d5aefc808cff52f68d6735
Max Windows <maximilianjentsch7@gmail.com>
2022-09-25 20:28:29 +0200

    Now delivering files *directly* to your home computer screen!

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,21 @@
+use std::env;
+use std::path::Path;
+
 #[async_std::main]
+
 async fn main() -> Result<(), std::io::Error> {
     femme::start();
+    let mut path = env::current_exe()?;
+
+    let mut index_path = path.join("..").join("..").join("..").join("html").join("index.html");
 
     let mut app = tide::new();
+    
     app.with(tide::log::LogMiddleware::new());
 
-    app.at("/").get(|_| async { Ok("Hello, world!") });
+    println!("{}", path.display());
+
+    app.at("/").serve_file(index_path);
 
     app.at("/test/").get(|_| async {Ok("test!")});
 

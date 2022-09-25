d4807d267104dadb3eb2cdf97bce174772ca9da0
Max Windows <maximilianjentsch7@gmail.com>
2022-09-25 19:51:21 +0200

    Server basics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hell, world!");
+#[async_std::main]
+async fn main() -> Result<(), std::io::Error> {
+    femme::start();
+
+    let mut app = tide::new();
+    app.with(tide::log::LogMiddleware::new());
+
+    app.at("/").get(|_| async { Ok("Hello, world!") });
+
+    app.at("/test/").get(|_| async {Ok("test!")});
+
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
 }

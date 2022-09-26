974b7ce82103a32cdbea58d16fbb470263f0ced5
Jerreu <jerome.reuschenbach2@gmail.com>
2022-09-26 23:13:13 +0200

    comments (who ever does those even?)

--- html/js/script.js
+++ html/js/script.js
@@ -1,6 +1,6 @@
-let productsPerLine = 3;
+let productsPerLine = 3; //Number of columns in the product grid
 
-function changeContrast() {
+function changeContrast() { //changes the contrast to the opposite, adds or removes the relevant variable to the URIs in all a tags, and in the adressbar
     const button = document.getElementById("contrastButton");
     const main = document.getElementsByTagName("main")[0];
     const body = document.body;
@@ -14,25 +14,25 @@ function changeContrast() {
         top.style.backgroundColor = secondColor;
         main.style.backgroundColor = secondColor;
         if (getUrlVars()["contrast"] == undefined) {
-            window.history.pushState("", "Estra Labs More Contrast", "?&contrast=1")
+            window.history.pushState("", "Estra Labs More Contrast", "?&contrast=1") //add variable to URI
         }
         for (i = 0; i < internalAs.length; i++) {
-            internalAs[i].href += "?&contrast=1";
+            internalAs[i].href += "?&contrast=1"; //add variable to a tags
         }
     } else {
         button.innerHTML = "More Contrast";
         body.style.backgroundColor = firstColor;
         top.style.backgroundColor = firstColor;
         main.style.backgroundColor = firstColor;
-        window.location.search = "";
+        window.location.search = ""; //remove variable from URI
         for (i = 0; i < internalAs.length; i++) {
-            internalAs[i].href = internalAs[i].href.slice(0, internalAs[i].href.indexOf('?'));
+            internalAs[i].href = internalAs[i].href.slice(0, internalAs[i].href.indexOf('?')); //remove variable from a tags
         }
     }
 }
 
 
-function getUrlVars() {
+function getUrlVars() { //returns variables of the URI as an object
     let vars = {};
     let parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function (m, key, value) {
         vars[key] = value;
@@ -40,7 +40,7 @@ function getUrlVars() {
     return vars;
 }
 
-function load() {
+function load() { //happens on site load
     checkContrast();
     updateYear();
     positionProducts();
@@ -52,11 +52,11 @@ function checkContrast() {
     }
 }
 
-function updateYear() {
+function updateYear() { //updates the year to the current one in the copyright statement
     document.getElementById("year").innerHTML = new Date().getFullYear();
 }
 
-function positionProducts() {
+function positionProducts() { //positions products on sites with products in a grid pattern with amount of columns given by productsPerLine
     const main = document.getElementsByTagName("main")[0];
     const products = document.getElementsByClassName("products");
     const productUl = document.getElementsByClassName("productsUl")[0];
@@ -64,15 +64,15 @@ function positionProducts() {
     let productUlGridTemplateColumns = "";
     for (let i = 0; i < productsPerLine; i++) {
         if (products[i]) {
-            productUlGridTemplateColumns += (((mainWidth - (productsPerLine * products[i].offsetWidth)) / (productsPerLine + 1)) + products[i].offsetWidth).toString() + "px ";
+            productUlGridTemplateColumns += (((mainWidth - (productsPerLine * products[i].offsetWidth)) / (productsPerLine + 1)) + products[i].offsetWidth).toString() + "px "; //add width of list Items to the Grid Template: left margin + itemWidth; produces a String to later be assigned to the style value
         }
     }
     for (let i = 0; i < products.length; i++) {
-        products[i].style.marginLeft = ((mainWidth - (productsPerLine * products[i].offsetWidth)) / (productsPerLine + 1)).toString() + "px";
+        products[i].style.marginLeft = ((mainWidth - (productsPerLine * products[i].offsetWidth)) / (productsPerLine + 1)).toString() + "px"; //calculate proper left margin to have this many items in one line with the same distance: Formula: margin = (totalWidth - columnNumber * itemWidth)/(columnNumber + 1)
     }
     productUl.style.gridTemplateColumns = productUlGridTemplateColumns;
 }
 
-function resize() {
+function resize() { //happens on site resize
     positionProducts();
 }

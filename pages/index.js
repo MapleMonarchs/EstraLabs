c8960bf3c2d5c292d48dd75ec09560476351dc52
Max Windows <maximilianjentsch7@gmail.com>
2022-11-23 21:57:13 +0100

    started migration to React and Next.js

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function HomePage() {
+
+    return (
+        <div>
+            <h1>UNDER CONSTRUCTION 👷‍♀️🏗🚧</h1>
+            <p>please stand by</p>
+        </div> 
+    );
+
+}

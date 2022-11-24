f6d4cec16ba5f5c975038c41f2a63b182aed9f61
= <=>
2022-11-24 12:53:01 +0100

    fixed image loading on pages

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,14 @@
 import Header from './Header'
+import Head from 'next/head';
 import styles from './layout.module.css'
 
 export default function Layout({ children }) {
     return (
         <div className={styles.back}>
+            <Head>
+                <link rel="shortcut icon" 
+                href="https://maplemonarchs.github.io/EstraLabs/images/favicon.ico"/>
+            </Head>
             <div>
                 <Header/>
             </div>

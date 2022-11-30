86a7739f123b200515fa6eff595c5d0235f0800b
= <=>
2022-11-30 10:00:43 +0100

    fixes

--- components/Layout.js
+++ components/Layout.js
@@ -2,6 +2,7 @@ import Header from './Header'
 import Head from 'next/head';
 import Image from "next/image";
 import styles from './layout.module.css'
+import { NextSeo } from 'next-seo';
 
 export default function Layout({ children }) {
     return (
@@ -9,11 +10,22 @@ export default function Layout({ children }) {
             <Head>
                 <link rel="shortcut icon" 
                 href="https://maplemonarchs.github.io/EstraLabs/images/favicon.ico"/>
-                <meta property="og:type" content="website" />
-                <meta property="og:title" content="EstraLabs" />
-                <meta property="og:url" content="https://maplemonarchs.github.io/EstraLabs" />
-                <meta property="og:image" content="https://maplemonarchs.github.io/EstraLabs/images/EstraLabs11-1.png" />
-                <meta property="og:description" content="uwu" />
+                <NextSeo
+                        title="EstraLabs"
+                        description="uwu"
+                        canonical="https://maplemonarchs.github.io/EstraLabs"
+                        openGraph={{
+                        url: 'https://maplemonarchs.github.io/EstraLabs',
+                        title: 'EstraLabs',
+                        description: 'uwu',
+                        site_name: 'EstraLabs',
+                        }}
+                        twitter={{
+                        handle: '@handle',
+                        site: '@site',
+                        cardType: 'summary_large_image',
+                        }}
+                        />
             </Head>
             <div>
                 <Header/>
